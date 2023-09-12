bdbd8863e4aed791401e54b0d656de0d3fd82b7d
Kesha <affenmilchmann@gmail.com>
2023-09-12 13:24:51 +0300

    shapes main comments

--- shapes_task/main.py
+++ shapes_task/main.py
@@ -3,12 +3,13 @@ from random import randint
 
 def main():
     shapes = []
+    # creating 1000 shapes with random type
     for _ in range(1000):
         if randint(0, 1):
             shapes.append(Triangle(randint(3, 5), randint(3, 5), randint(3, 5)))
         else:
             shapes.append(Circle(randint(1, 15)))
-
+    # counting stats
     total_triangles = total_circles = 0
     total_area = triangle_area_sum = circle_area_sum = 0
     total_perimeter = triangle_perimeter_sum = circle_perimeter_sum = 0
@@ -28,7 +29,7 @@ def main():
             total_circles += 1
             circle_area_sum += area
             circle_perimeter_sum += perimeter
-
+    # printing stats
     print(f"""    Total shapes: {len(shapes)}
         Avg area: {total_area / len(shapes):.2f}
         Avg perimeter: {total_perimeter / len(shapes):.2f}
